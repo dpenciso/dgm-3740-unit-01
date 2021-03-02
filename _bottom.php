54b20cfc45261acb79321d11881965447ff74c72
David Enciso <dpenciso@msn.com>
2021-03-01 19:13:26 -0700

    broke up home page using php

---
+++ _bottom.php
@@ -0,0 +1,76 @@
+</main>
+
+    <!-- FOOTER STARTS HERE -->
+    <div id="footerWrapper">
+      <footer class="clearfix">
+        <div class="footer-sections">
+          <div class="footer-div">
+            <h1>Contact Us</h1>
+            <p>CABOT CRUIZES<br><a href="https://goo.gl/maps/FzY3fyLQFv4NAC1k9">333 Brannan St.<br>San Francisco, CA 94107</a></p>
+            <h1>Phone:</h1>
+            <p><a href="tel:1-800-123-456">1-800-123-4567</a></p>
+            <h1>Hours of Operation:</h1>
+            <p>Monday-Friday 9am-4pm</p>
+          </div>
+          <div class="footer-div">
+            <h1>News Letter</h1>
+            <p>Subscribe to our email list and stay up-to-date with our hottest offers and latest specials.</p>
+            <form action="submit"><input type="email" required><input type="submit" value="Subscribe"></form>
+          </div>
+          <div class="footer-div">
+            <h1>Why Cabot Cruises</h1>
+            <p>Cabot Cruises is a travel agency providing the best cruise deals on our online travel website. We can help you with all inclusive vacations including discounted cruises.</p>
+            <p>We do not sell travel to residents of Delaware, Iowa, Florida, Hawaii and Washington state because those states are just weird. If you are a resident of one of these states, call your congressman and tell them to change the regula8ons.</p>
+          </div>
+        </div>
+        <div class="copyright">
+          <div class="social"><i class="fab fa-instagram"></i><i class="fab fa-facebook-f"></i><i class="fab fa-twitter"></i><i class="fab fa-pinterest"></i><i class="fab fa-tumblr"></i></div>
+          <h1><a href="https://www.davidenciso.com/">David Enciso</a> &copy; 2021</h1>
+        </div>
+      </footer>
+    </div>
+    <!-- end footer wrapper-->
+
+    <script src="js/scripts.js"></script>
+    <script
+      src="https://kit.fontawesome.com/82e4275e9f.js"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      type="text/javascript"
+      src="//code.jquery.com/jquery-1.11.0.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="//code.jquery.com/jquery-migrate-1.2.1.min.js"
+    ></script>
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+    <script>
+      $(document).ready(function () {
+        $(".your-class").slick({
+          dots: true,
+        });
+      });
+    </script>
+    <script src="https://kit.fontawesome.com/82e4275e9f.js" crossorigin="anonymous"></script>
+    <script type="application/ld+json">
+      {
+        "@context": "http://schema.org",
+        "@type": "TravelAgency",
+        "name": "Cabot Cruises",
+        "description": "Cabot Cruises is a travel agency providing the best cruise deals on our online travel website",
+        "openingHours": "Mo-Fr 09:00-16:00",
+        "telephone": "+18011234567",
+        "faxNumber": "+18017654321",
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "333 Brannan St.",
+          "addressLocality": "San Francisco",
+          "addressRegion": "CA"
+        },
+        "image": "https://www.davidenciso.com/images/avatar.png",
+        "priceRange": "$500 to $4000"
+      }
+    </script>
+  </body>
+</html>
